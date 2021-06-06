4a0ebf5eaf65fc946c5bb4367fc32e56eeb7e942
Diatemba99 <martin.diatta99@gmail.com>
2021-06-06 22:26:19 +0000

    adding card group

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  todoList = [{
+    itemName : 'Coding',
+    itemDueDate : '13-01-21',
+    itemPriority: 'High',
+    itemCategory: 'Work'
+  },
+  {
+    itemName : 'Design',
+    itemDueDate : '25-05-21',
+    itemPriority: 'Middle',
+    itemCategory: 'Business'
+  },
+  {
+    itemName : 'Analyse',
+    itemDueDate : '03-04-21',
+    itemPriority: 'Low',
+    itemCategory: 'Personnal'
+  },
+  {
+    itemName : 'Examen',
+    itemDueDate : '03-04-21',
+    itemPriority: 'Middle',
+    itemCategory: 'Group'
   }
+]
+
+  today : number = Date.now();
+
+  constructor(public navCtrl: NavController) {  }
 
 }
